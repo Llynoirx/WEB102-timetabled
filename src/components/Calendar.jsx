142bb2353a8d11cb4405dc32f163cfe057b19105
llynoirx <kathyho04@gmail.com>
2024-09-23 18:37:52 -0400

    fixed thead (checkpoint 3) and finished checkpoint 4

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,26 +1,26 @@
 import React from "react";
+import Event from './Event'
 
 const Calendar = () => {
-
-
     return (
         <div className="Calendar">
            <table>
-            <thead>
-                <tr>
-                    <th></th>
-                    <th>Sunday</th>
-                    <th>Monday</th>
-                    <th>Tuesday</th>
-                    <th>Wednesday</th>
-                    <th>Thursday</th>
-                    <th>Friday</th>
-                    <th>Saturday</th>
-                </tr>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Sunday</th>
+                        <th>Monday</th>
+                        <th>Tuesday</th>
+                        <th>Wednesday</th>
+                        <th>Thursday</th>
+                        <th>Friday</th>
+                        <th>Saturday</th>
+                    </tr>
+                </thead>
                 <tbody>
                     <tr>
                         <td className="time">8am</td>
-                        <td></td>
+                        <Event />
                         <td></td>
                         <td></td>
                         <td></td>
@@ -119,7 +119,6 @@ const Calendar = () => {
                         <td></td>
                     </tr>
                 </tbody>
-            </thead>
            </table>
         </div>
     )
---
+++ src/components/Event.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Event = (props) =>  {
+    return (
+        <td className="Event">
+            <h5>
+                Test Event
+            </h5>
+        </td>
+    )
+}
+
+export default Event;
